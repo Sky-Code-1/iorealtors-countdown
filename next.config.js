d17b2cff7ea019dc1d364564e8a10fd5a89f2976
Isaiah Osarobo <Isaiahosarobo6@gmail.com>
2025-07-01 22:02:13 +0100

    Implemented config as js file

---
+++ next.config.js
@@ -0,0 +1,7 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // This tells Next.js to use `next export` and generate static HTML into the `out` directory
+};
+
+module.exports = nextConfig;
